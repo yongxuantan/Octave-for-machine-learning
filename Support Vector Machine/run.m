b903ba506ad3583c69a0ee8abc362c6191b87bc1
Mike Tan <ymtan1@hotmail.com>
2020-05-20 03:05:45 -0500

    Example use of SVM

---
+++ Support Vector Machine/run.m
@@ -0,0 +1,475 @@
+% Run file for an example Support Vector Machines
+% programmer: the one and only :D
+% 
+% data: 
+% - ex6data1.mat, ex6data2.mat, ex6data3.mat
+%	  - are sets of coordinates and their label in native Octave matrix format
+%     - each coordinate contains X1 and X2
+%     - label y for positive or negative
+%     - ex6data3.mat also have a subset of data points for validation
+% - spamTrain.mat, spamTest.mat
+%	  - training and test spam emails and their label in native Octave format
+%
+% figures:
+% - svm1.png: dataset1 with trained linear SVM model
+% - svm2.png: dataset2 with trained SVM model
+% - svm3.png: dataset3 with trained SVM model
+%
+% goal: implement SVM and train models for binary classification
+% - including feature extraction from sample emails
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% ----------------------------------------- %%
+function visualizeBoundaryLinear(X, y, model)
+%VISUALIZEBOUNDARYLINEAR plots a linear decision boundary learned by the
+%SVM
+%   VISUALIZEBOUNDARYLINEAR(X, y, model) plots a linear decision boundary 
+%   learned by the SVM and overlays the data on it
+
+w = model.w;
+b = model.b;
+xp = linspace(min(X(:,1)), max(X(:,1)), 100);
+yp = - (w(1)*xp + b)/w(2);
+plotData(X, y);
+hold on;
+plot(xp, yp, '-b'); 
+hold off
+
+end
+% ----------------------------------------- %
+
+%% ----------------------------------------- %%
+function visualizeBoundary(X, y, model, varargin)
+%VISUALIZEBOUNDARY plots a non-linear decision boundary learned by the SVM
+%   VISUALIZEBOUNDARYLINEAR(X, y, model) plots a non-linear decision 
+%   boundary learned by the SVM and overlays the data on it
+
+% Plot the training data on top of the boundary
+plotData(X, y)
+
+% Make classification predictions over a grid of values
+x1plot = linspace(min(X(:,1)), max(X(:,1)), 100)';
+x2plot = linspace(min(X(:,2)), max(X(:,2)), 100)';
+[X1, X2] = meshgrid(x1plot, x2plot);
+vals = zeros(size(X1));
+for i = 1:size(X1, 2)
+   this_X = [X1(:, i), X2(:, i)];
+   vals(:, i) = svmPredict(model, this_X);
+end
+
+% Plot the SVM boundary
+hold on
+contour(X1, X2, vals, [0.5 0.5], 'b');
+hold off;
+
+end
+% ----------------------------------------- %
+
+
+%% ==================== Training Linear SVM ====================
+%  The following code will train a linear SVM on the dataset and plot the
+%  decision boundary learned.
+%
+
+%% ---------------------------------------------------------------- %%
+function sim = linearKernel(x1, x2)
+%LINEARKERNEL returns a linear kernel between x1 and x2
+%   sim = linearKernel(x1, x2) returns a linear kernel between x1 and x2
+%   and returns the value in sim
+
+% Ensure that x1 and x2 are column vectors
+x1 = x1(:); x2 = x2(:);
+
+% Compute the kernel
+sim = x1' * x2;  % dot product
+end
+% ---------------------------------------------------------------- %
+
+% Load from ex6data1: 
+% You will have X, y in your environment
+load('ex6data1.mat');
+
+fprintf('\nTraining Linear SVM ...\n')
+
+% You should try to change the C value below and see how the decision
+% boundary varies (e.g., try C = 1000)
+C = 1;
+model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
+visualizeBoundaryLinear(X, y, model);
+
+
+%% =============== Part 3: Implementing Gaussian Kernel ===============
+%  You will now implement the Gaussian kernel to use
+%  with the SVM. 
+%
+
+%% ---------------------------------------------------------------- %%
+function sim = gaussianKernel(x1, x2, sigma)
+%RBFKERNEL returns a radial basis function kernel between x1 and x2
+%   sim = gaussianKernel(x1, x2) returns a gaussian kernel between x1 and x2
+%   and returns the value in sim
+
+% Ensure that x1 and x2 are column vectors
+x1 = x1(:); x2 = x2(:);
+sim = 0;
+diff = x1 - x2;
+sim = exp(- diff' * diff / (2 * sigma^2));
+end
+% ---------------------------------------------------------------- %
+
+fprintf('\nEvaluating the Gaussian Kernel ...\n')
+
+x1 = [1 2 1]; x2 = [0 4 -1]; sigma = 2;
+sim = gaussianKernel(x1, x2, sigma);
+
+fprintf(['Gaussian Kernel between x1 = [1; 2; 1], x2 = [0; 4; -1], sigma = %f :' ...
+         '\n\t%f\n(for sigma = 2, this value should be about 0.324652)\n'], sigma, sim);
+
+
+%% ========== Training SVM with RBF Kernel (Dataset 2) ==========
+%  After you have implemented the kernel, we can now use it to train the 
+%  SVM classifier.
+% 
+fprintf('\nTraining SVM with RBF Kernel (this may take 1 to 2 minutes) ...\n');
+
+% Load from ex6data2: 
+% You will have X, y in your environment
+load('ex6data2.mat');
+
+% SVM Parameters
+C = 1; sigma = 0.1;
+
+% We set the tolerance and max_passes lower here so that the code will run
+% faster. However, in practice, you will want to run the training to
+% convergence.
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+visualizeBoundary(X, y, model);
+
+
+%% ========== Part 7: Training SVM with RBF Kernel (Dataset 3) ==========
+
+%  This is a different dataset that you can use to experiment with. Try
+%  different values of C and sigma here.
+% 
+
+% Load from ex6data3: 
+% You will have X, y in your environment
+load('ex6data3.mat');
+
+%% ---------------------------------------------------------------- %%
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% initialize return variables
+C = 1;
+sigma = 0.3;
+
+num = 8;
+multi = 3.16;
+CList = zeros(num,1);
+SList = zeros(num,1);
+for i=1:length(CList)
+	CList(i) = 0.01*multi^(i-1);
+	SList(i) = 0.01*multi^(i-1);
+end;
+
+predictError = zeros(length(CList),length(SList));
+
+for r=1:size(predictError,1)
+	for s=1:size(predictError,2)
+		model= svmTrain(X, y, CList(r), @(x1, x2) gaussianKernel(x1, x2, SList(s)));
+		predictions = svmPredict(model, Xval);
+		predictError(r,s) = mean(double(predictions ~= yval));
+	end;
+end;
+
+minError = min(min(predictError));
+[a,b] = find(predictError == minError);
+
+C = CList(a(1));
+sigma = SList(b(1));
+
+end
+% ---------------------------------------------------------------- %
+
+% Try different SVM Parameters here
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+
+% Train the SVM
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+visualizeBoundary(X, y, model);
+
+
+
+
+%% Initialization
+clear ; close all; clc
+
+%% ----------------------------------------- %%
+function file_contents = readFile(filename)
+%READFILE reads a file and returns its entire contents 
+%   file_contents = READFILE(filename) reads a file and returns its entire
+%   contents in file_contents
+%
+
+% Load File
+fid = fopen(filename);
+if fid
+    file_contents = fscanf(fid, '%c', inf);
+    fclose(fid);
+else
+    file_contents = '';
+    fprintf('Unable to open %s\n', filename);
+end
+
+end
+% ----------------------------------------- %
+
+%% ---------------------------------------------------------------- %%
+function word_indices = processEmail(email_contents)
+%PROCESSEMAIL preprocesses a the body of an email and
+%returns a list of word_indices 
+%   word_indices = PROCESSEMAIL(email_contents) preprocesses 
+%   the body of an email and returns a list of indices of the 
+%   words contained in the email. 
+%
+
+% Load Vocabulary
+vocabList = getVocabList();
+
+% Init return value
+word_indices = [];
+
+% ========================== Preprocess Email ===========================
+
+% Find the Headers ( \n\n and remove )
+% Uncomment the following lines if you are working with raw emails with the
+% full headers
+
+% hdrstart = strfind(email_contents, ([char(10) char(10)]));
+% email_contents = email_contents(hdrstart(1):end);
+
+% Lower case
+email_contents = lower(email_contents);
+
+% Strip all HTML
+% Looks for any expression that starts with < and ends with > and replace
+% and does not have any < or > in the tag it with a space
+email_contents = regexprep(email_contents, '<[^<>]+>', ' ');
+
+% Handle Numbers
+% Look for one or more characters between 0-9
+email_contents = regexprep(email_contents, '[0-9]+', 'number');
+
+% Handle URLS
+% Look for strings starting with http:// or https://
+email_contents = regexprep(email_contents, ...
+                           '(http|https)://[^\s]*', 'httpaddr');
+
+% Handle Email Addresses
+% Look for strings with @ in the middle
+email_contents = regexprep(email_contents, '[^\s]+@[^\s]+', 'emailaddr');
+
+% Handle $ sign
+email_contents = regexprep(email_contents, '[$]+', 'dollar');
+
+
+% ========================== Tokenize Email ===========================
+
+% Output the email to screen as well
+fprintf('\n==== Processed Email ====\n\n');
+
+% Process file
+l = 0;
+
+while ~isempty(email_contents)
+
+    % Tokenize and also get rid of any punctuation
+    [str, email_contents] = ...
+       strtok(email_contents, ...
+              [' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+   
+    % Remove any non alphanumeric characters
+    str = regexprep(str, '[^a-zA-Z0-9]', '');
+
+    % Stem the word 
+    % (the porterStemmer sometimes has issues, so we use a try catch block)
+    try str = porterStemmer(strtrim(str)); 
+    catch str = ''; continue;
+    end;
+
+    % Skip the word if it is too short
+    if length(str) < 1
+       continue;
+    end
+
+    % Look up the word in the dictionary and add to word_indices if
+    % found
+    % 
+    % Note: vocabList{idx} returns a the word with index idx in the
+    %       vocabulary list.
+    %
+
+	for i=1:length(vocabList)
+		if strcmp(str, vocabList{i})
+			word_indices = [word_indices ; i];
+			break;
+		end
+	end;
+
+    % =============================================================
+
+    % Print to screen, ensuring that the output lines are not too long
+    if (l + length(str) + 1) > 78
+        fprintf('\n');
+        l = 0;
+    end
+    fprintf('%s ', str);
+    l = l + length(str) + 1;
+
+end
+
+% Print footer
+fprintf('\n\n=========================\n');
+
+end
+% ---------------------------------------------------------------- %
+
+%% ==================== Email Preprocessing ====================
+%  To use an SVM to classify emails into Spam v.s. Non-Spam, you first need
+%  to convert each email into a vector of features. 
+
+fprintf('\nPreprocessing sample email (emailSample1.txt)\n');
+
+% Extract Features
+file_contents = readFile('emailSample1.txt');
+word_indices  = processEmail(file_contents);
+
+% Print Stats
+fprintf('Word Indices: \n');
+fprintf(' %d', word_indices);
+fprintf('\n\n');
+
+
+%% ==================== Feature Extraction ====================
+%  Now, you will convert each email into a vector of features in R^n. 
+
+%% ---------------------------------------------------------------- %%
+function x = emailFeatures(word_indices)
+%EMAILFEATURES takes in a word_indices vector and produces a feature vector
+%from the word indices
+%   x = EMAILFEATURES(word_indices) takes in a word_indices vector and 
+%   produces a feature vector from the word indices. 
+
+% Total number of words in the dictionary
+n = 1899;
+
+% initialize return value
+x = zeros(n, 1);
+% 
+%               Concretely, if an email has the text:
+%
+%                  The quick brown fox jumped over the lazy dog.
+%
+%               Then, the word_indices vector for this text might look 
+%               like:
+%               
+%                   60  100   33   44   10     53  60  58   5
+%
+%               where, we have mapped each word onto a number, for example:
+%
+%                   the   -- 60
+%                   quick -- 100
+%                   ...
+%
+%              (note: the above numbers are just an example and are not the
+%               actual mappings).
+%
+%              take one such word_indices vector and construct
+%              a binary feature vector that indicates whether a particular
+%              word occurs in the email. That is, x(i) = 1 when word i
+%              is present in the email. Concretely, if the word 'the' (say,
+%              index 60) appears in the email, then x(60) = 1. The feature
+%              vector should look like:
+%
+%              x = [ 0 0 0 0 1 0 0 0 ... 0 0 0 0 1 ... 0 0 0 1 0 ..];
+%
+%
+
+for j=1:length(word_indices)
+	x(word_indices(j))=1;
+end;
+
+end
+
+% ---------------------------------------------------------------- %
+
+fprintf('\nExtracting features from sample email (emailSample1.txt)\n');
+
+% Extract Features
+file_contents = readFile('emailSample1.txt');
+word_indices  = processEmail(file_contents);
+features      = emailFeatures(word_indices);
+
+% Print Stats
+fprintf('Length of feature vector: %d\n', length(features));
+fprintf('Number of non-zero entries: %d\n', sum(features > 0));
+
+
+%% =========== Train Linear SVM for Spam Classification ========
+%  In this section, you will train a linear classifier to determine if an
+%  email is Spam or Not-Spam.
+
+% Load the Spam Email dataset
+% You will have X, y in your environment
+load('spamTrain.mat');
+
+fprintf('\nTraining Linear SVM (Spam Classification)\n')
+fprintf('(this may take 1 to 2 minutes) ...\n')
+
+C = 0.1;
+model = svmTrain(X, y, C, @linearKernel);
+
+p = svmPredict(model, X);
+
+fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
+
+%% =================== Test Spam Classification ================
+%  After training the classifier, we can evaluate it on a test set. We have
+%  included a test set in spamTest.mat
+
+% Load the test dataset
+% You will have Xtest, ytest in your environment
+load('spamTest.mat');
+
+fprintf('\nEvaluating the trained Linear SVM on a test set ...\n')
+
+p = svmPredict(model, Xtest);
+
+fprintf('Test Accuracy: %f\n', mean(double(p == ytest)) * 100);
+
+%% ================= Top Predictors of Spam ====================
+%  Since the model we are training is a linear SVM, we can inspect the
+%  weights learned by the model to understand better how it is determining
+%  whether an email is spam or not. The following code finds the words with
+%  the highest weights in the classifier. Informally, the classifier
+%  'thinks' that these words are the most likely indicators of spam.
+%
+
+% Sort the weights and obtin the vocabulary list
+[weight, idx] = sort(model.w, 'descend');
+vocabList = getVocabList();
+
+fprintf('\nTop predictors of spam: \n');
+for i = 1:15
+    fprintf(' %-15s (%f) \n', vocabList{idx(i)}, weight(i));
+end
