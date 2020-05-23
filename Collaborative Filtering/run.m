0c71851d0971601f355cf02a35156169aafdc92f
Mike Tan <ymtan1@hotmail.com>
2020-05-23 00:10:15 -0500

    Example use of Collaborative Filtering

---
+++ Collaborative Filtering/run.m
@@ -0,0 +1,289 @@
+% Run file for an example Collaborative Filtering
+% programmer: the one and only :D
+% 
+% data: 
+% - ex8_movies.mat: matrix type data in native Octave format
+%   - Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies on  943 users
+%   - R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a rating to movie i, 0 otherwise
+% - ex8_movieParams.mat: data with pre-trained weights in native octave format
+%   - contains: X, Theta, num_users, num_movies, num_features
+% - movie_idx.txt: list of all movies and their number
+%
+% goals: Recommender Systems
+% 	- implement the collaborative filtering learning algorithm and apply it to a dataset of movie ratings
+
+%% Initialization
+clear ; close all; clc
+
+%% ----------------------------------------- %%
+function movieList = loadMovieList()
+%GETMOVIELIST reads the fixed movie list in movie.txt and returns a
+%cell array of the words
+%   movieList = GETMOVIELIST() reads the fixed movie list in movie.txt 
+%   and returns a cell array of the words in movieList.
+
+
+%% Read the fixed movieulary list
+fid = fopen('movie_ids.txt');
+
+% Store all movies in cell array movie{}
+n = 1682;  % Total number of movies 
+
+movieList = cell(n, 1);
+for i = 1:n
+    % Read line
+    line = fgets(fid);
+    % Word Index (can ignore since it will be = i)
+    [idx, movieName] = strtok(line, ' ');
+    % Actual Word
+    movieList{i} = strtrim(movieName);
+end
+fclose(fid);
+
+end
+% ----------------------------------------- %
+
+%% =============== Loading movie ratings dataset ================
+%  You will start by loading the movie ratings dataset to understand the
+%  structure of the data.
+%  
+fprintf('Loading movie ratings dataset.\n\n');
+
+%  Load data
+load ('ex8_movies.mat');
+
+%  Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies on 
+%  943 users
+%
+%  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
+%  rating to movie i
+
+%  From the matrix, we can compute statistics like average rating.
+fprintf('Average rating for movie 1 (Toy Story): %f / 5\n\n', ...
+        mean(Y(1, R(1, :))));
+
+%  We can "visualize" the ratings matrix by plotting it with imagesc
+imagesc(Y);
+ylabel('Movies');
+xlabel('Users');
+
+
+%% ============ Collaborative Filtering Cost Function ===========
+%  To help you debug your cost function, we have included set of weights
+%  that we trained on that.
+
+%% ---------------------------------------------------------------- %%
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
+                                  num_features, lambda)
+%COFICOSTFUNC Collaborative filtering cost function
+%   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+%   num_features, lambda) returns the cost and gradient for the
+%   collaborative filtering problem.
+%
+
+% Unfold the U and W matrices from params
+X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), ...
+                num_users, num_features);
+            
+% initialize return parameters
+J = 0;
+X_grad = zeros(size(X));
+Theta_grad = zeros(size(Theta));
+
+J = 1/2 * sum(sum(((X*Theta' .* R) - Y).^2)) + lambda/2 * sum(sum(Theta.^2)) + lambda/2 * sum(sum(X.^2));
+
+X_grad = ((X*Theta' .* R) - Y)*Theta + lambda*X;
+Theta_grad = ((X*Theta' .* R) - Y)'*X + lambda*Theta;
+grad = [X_grad(:); Theta_grad(:)];
+
+end
+% ---------------------------------------------------------------- %
+
+%  Load pre-trained weights (X, Theta, num_users, num_movies, num_features)
+load ('ex8_movieParams.mat');
+
+%  Reduce the data set size so that this runs faster
+num_users = 4; num_movies = 5; num_features = 3;
+X = X(1:num_movies, 1:num_features);
+Theta = Theta(1:num_users, 1:num_features);
+Y = Y(1:num_movies, 1:num_users);
+R = R(1:num_movies, 1:num_users);
+
+%  Evaluate cost function
+J = cofiCostFunc([X(:) ; Theta(:)], Y, R, num_users, num_movies, ...
+               num_features, 0);
+           
+fprintf(['Cost at loaded parameters: %f '...
+         '\n(this value should be about 22.22)\n'], J);
+
+
+%% ============== Collaborative Filtering Gradient ==============
+%  Once your cost function matches up with ours, you should now implement 
+%  the collaborative filtering gradient function. Specifically, you should 
+%  complete the code in cofiCostFunc.m to return the grad argument.
+%  
+fprintf('\nChecking Gradients (without regularization) ... \n');
+
+%  Check gradients by running checkNNGradients
+checkCostFunction;
+
+
+%% ========= Collaborative Filtering Cost Regularization ========
+%  Implement regularization for the cost function for 
+%  collaborative filtering. 
+%  
+
+%  Evaluate cost function
+J = cofiCostFunc([X(:) ; Theta(:)], Y, R, num_users, num_movies, ...
+               num_features, 1.5);
+           
+fprintf(['Cost at loaded parameters (lambda = 1.5): %f '...
+         '\n(this value should be about 31.34)\n'], J);
+
+
+%% ======= Collaborative Filtering Gradient Regularization ======
+%  Implement regularization for the gradient. 
+%
+
+fprintf('\nChecking Gradients (with regularization) ... \n');
+
+%  Check gradients by running checkNNGradients
+checkCostFunction(1.5);
+
+
+%% ============== Entering ratings for a new user ===============
+%  Before we will train the collaborative filtering model, we will first
+%  add ratings that correspond to a new user that we just observed. This
+%  part of the code will also allow you to put in your own ratings for the
+%  movies in our dataset!
+%
+movieList = loadMovieList();
+
+%  Initialize my ratings
+my_ratings = zeros(1682, 1);
+
+% Check the file movie_idx.txt for id of each movie in our dataset
+% For example, Toy Story (1995) has ID 1, so to rate it "4", you can set
+my_ratings(1) = 4;
+
+% Or suppose did not enjoy Silence of the Lambs (1991), you can set
+my_ratings(98) = 2;
+
+% We have selected a few movies we liked / did not like and the ratings we
+% gave are as follows:
+my_ratings(7) = 3;
+my_ratings(12)= 5;
+my_ratings(54) = 4;
+my_ratings(64)= 5;
+my_ratings(66)= 3;
+my_ratings(69) = 5;
+my_ratings(183) = 4;
+my_ratings(226) = 5;
+my_ratings(355)= 5;
+
+fprintf('\n\nNew user ratings:\n');
+for i = 1:length(my_ratings)
+    if my_ratings(i) > 0 
+        fprintf('Rated %d for %s\n', my_ratings(i), ...
+                 movieList{i});
+    end
+end
+
+
+%% ================== Learning Movie Ratings ====================
+%  Now, you will train the collaborative filtering model on a movie rating 
+%  dataset of 1682 movies and 943 users
+%
+
+%% ----------------------------------------- %%
+function [Ynorm, Ymean] = normalizeRatings(Y, R)
+%NORMALIZERATINGS Preprocess data by subtracting mean rating for every 
+%movie (every row)
+%   [Ynorm, Ymean] = NORMALIZERATINGS(Y, R) normalized Y so that each movie
+%   has a rating of 0 on average, and returns the mean rating in Ymean.
+%
+
+[m, n] = size(Y);
+Ymean = zeros(m, 1);
+Ynorm = zeros(size(Y));
+for i = 1:m
+    idx = find(R(i, :) == 1);
+    Ymean(i) = mean(Y(i, idx));
+    Ynorm(i, idx) = Y(i, idx) - Ymean(i);
+end
+end
+% ----------------------------------------- %
+
+fprintf('\nTraining collaborative filtering...\n');
+
+%  Load data
+load('ex8_movies.mat');
+
+%  Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies by 
+%  943 users
+%
+%  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
+%  rating to movie i
+
+%  Add our own ratings to the data matrix
+Y = [my_ratings Y];
+R = [(my_ratings ~= 0) R];
+
+%  Normalize Ratings
+[Ynorm, Ymean] = normalizeRatings(Y, R);
+
+%  Useful Values
+num_users = size(Y, 2);
+num_movies = size(Y, 1);
+num_features = 10;
+
+% Set Initial Parameters (Theta, X)
+X = randn(num_movies, num_features);
+Theta = randn(num_users, num_features);
+
+initial_parameters = [X(:); Theta(:)];
+
+% Set options for fmincg
+options = optimset('GradObj', 'on', 'MaxIter', 100);
+
+% Set Regularization
+lambda = 10;
+theta = fmincg (@(t)(cofiCostFunc(t, Ynorm, R, num_users, num_movies, ...
+                                num_features, lambda)), ...
+                initial_parameters, options);
+
+% Unfold the returned theta back into U and W
+X = reshape(theta(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(theta(num_movies*num_features+1:end), ...
+                num_users, num_features);
+
+fprintf('Recommender system learning completed.\n');
+
+
+%% ================== Recommendation for you ====================
+%  After training the model, you can now make recommendations by computing
+%  the predictions matrix.
+%
+
+p = X * Theta';
+my_predictions = p(:,1) + Ymean;
+
+movieList = loadMovieList();
+
+[r, ix] = sort(my_predictions, 'descend');
+fprintf('\nTop recommendations for you:\n');
+for i=1:10
+    j = ix(i);
+    fprintf('Predicting rating %.1f for movie %s\n', my_predictions(j), ...
+            movieList{j});
+end
+
+fprintf('\n\nOriginal ratings provided:\n');
+for i = 1:length(my_ratings)
+    if my_ratings(i) > 0 
+        fprintf('Rated %d for %s\n', my_ratings(i), ...
+                 movieList{i});
+    end
+end
+
